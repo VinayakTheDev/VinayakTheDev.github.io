d09221202826b80325298abcdc4f497c7d5fc659
Vinayak Deshpande <vinayak.deshpande@vedantu.com>
2020-10-11 20:04:18 +0530

    Updates

---
+++ precache-manifest.d00e3de589d1c7451a8c7c512c3249a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3de5f1d8d58116154351a5b25693e26f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "783f2299ee6a9ab6e9fe",
+    "url": "/static/css/main.013078cf.chunk.css"
+  },
+  {
+    "revision": "ca954a91f2acf13d7bda",
+    "url": "/static/js/2.b2f1af51.chunk.js"
+  },
+  {
+    "revision": "783f2299ee6a9ab6e9fe",
+    "url": "/static/js/main.838b6995.chunk.js"
+  },
+  {
+    "revision": "849829d641ebe2ccab68",
+    "url": "/static/js/runtime-main.88d82d10.js"
+  }
+]);
